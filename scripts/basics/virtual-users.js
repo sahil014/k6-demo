c65efdfdd88c46f195baecdff294bf408caca230
Sahil Narang <sahil7224@Sahils-MacBook-Air.local>
2026-06-29 21:41:37 +0530

    updated virtual users file

--- scripts/basics/virtual-users.js
+++ scripts/basics/virtual-users.js
@@ -1,8 +1,8 @@
 import http from 'k6/http'
-export const options={
-    vus:5,
-    duration:'10s'
-}
+export const options = {
+    vus: 20,
+    duration: '15s'
+};
 
 export default function(){
     http.get('https://test.k6.io');
